284ca97b76c196ab48d9368baa938b7566411851
Scott Oswald <scottoswald1@gmail.com>
2026-02-10 17:32:04 +0000

    exercise - 1-javascript/exercise/checkMe.js

--- 1-javascript/exercise/checkMe.js
+++ 1-javascript/exercise/checkMe.js
@@ -9,18 +9,19 @@ let language = {
 
 console.log(language); // { name: 'JavaScript', ... }
 console.log(language.officialName); // ECMAScript
-console.log(language.creater); // undefined
+console.log(language.creator); // undefined
 
 
-language = 'TypeScript'; // String replacing object? JS don't care!
+language.name = 'TypeScript'; // String replacing object? JS don't care!
 // Setting random properties on a String? (non-strict) JS don't care!
 language.released = 2012;
 language.creator = 'Anders Hejlsberg';
 language.company = 'Microsoft';
+language.officialName = language.name
 
 console.log(language); // TypeScript
 console.log(language.released); // undefined
-console.lgo(language.company); // TypeError: console.lgo is not a function
+console.log(language.company); // TypeError: console.lgo is not a function
 console.log(language.officialName); // undefined
 
 
