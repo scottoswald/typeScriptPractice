a97bf68162e90a44e63a499e54cdb36974f21059
Scott Oswald <scottoswald1@gmail.com>
2026-02-20 09:58:51 +0000

    changes to exercise 2 file, using compiling flags

--- 2-annotations/exercise/typeMe.js
+++ 2-annotations/exercise/typeMe.js
@@ -1,7 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getEventDetails = void 0;
-var events = [
+const events = [
     {
         id: "1",
         title: "Thanksgiving Potluck",
@@ -20,54 +17,53 @@ var events = [
 ];
 // Should return an event object, or null if not found
 function getEventById(id) {
-    return events.filter(function (e) { return e.id.toString() === id.toString(); })[0];
+    return events.filter(e => e.id.toString() === id.toString())[0];
 }
 // Should return an object with dateString & isPast 
 function getEventDate(event) {
-    var eventDate = new Date(event.date);
-    var dateString = eventDate.toDateString();
-    var isPast = eventDate < new Date();
-    return { dateString: dateString, isPast: isPast };
+    const eventDate = new Date(event.date);
+    const dateString = eventDate.toDateString();
+    const isPast = eventDate < new Date();
+    return { dateString, isPast };
 }
 // Should return a string like '5 going' or '0 went'
-var getEventRsvpCount = function (event) {
-    var count = event.rsvps ? event.rsvps.length : 0;
-    var isPast = getEventDate(event).isPast;
-    var text = isPast ? 'went' : 'going';
+const getEventRsvpCount = (event) => {
+    const count = event.rsvps ? event.rsvps.length : 0;
+    const { isPast } = getEventDate(event);
+    const text = isPast ? 'went' : 'going';
     return [count, text].join(' ');
 };
 // Should return a string with the event's title, date, and rsvps
 // (if the event exists), or the string 'Event not found' (if not)
-var getEventDetails = function (eventId) {
-    var event = getEventById(eventId);
+const getEventDetails = (eventId) => {
+    const event = getEventById(eventId);
     if (event) {
-        var dateString = getEventDate(event).dateString;
-        var eventRsvps = getEventRsvpCount(event);
-        return "".concat(event.title, " on ").concat(dateString, ": ").concat(eventRsvps);
+        const { dateString } = getEventDate(event);
+        const eventRsvps = getEventRsvpCount(event);
+        return `${event.title} on ${dateString}: ${eventRsvps}`;
     }
     else {
         return "Event not found";
     }
     ;
 };
-exports.getEventDetails = getEventDetails;
 function test() {
-    var results = [{
+    const results = [{
             actual: getEventDetails(1),
-            expected: "".concat(events[0].title, " on ").concat(new Date(events[0].date).toDateString(), ": 3 going"),
+            expected: `${events[0].title} on ${new Date(events[0].date).toDateString()}: 3 going`,
         },
         {
             actual: getEventDetails(2),
-            expected: "".concat(events[1].title, " on ").concat(new Date(events[1].date).toDateString(), ": 0 going"),
+            expected: `${events[1].title} on ${new Date(events[1].date).toDateString()}: 0 going`,
         },
         {
             actual: getEventDetails(404),
             expected: 'Event not found',
         }];
-    for (var _i = 0, results_1 = results; _i < results_1.length; _i++) {
-        var result = results_1[_i];
-        var label = result.expected === result.actual ? '✅' : '❌';
+    for (let result of results) {
+        const label = result.expected === result.actual ? '✅' : '❌';
         console.log(label, result.actual);
     }
 }
 test();
+export { getEventDetails };
